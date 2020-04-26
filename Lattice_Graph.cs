8ca771839dfc52e57a6efcdca66fb626c0be480d
Jesse Raffield <jhraffield@gmail.com>
2020-04-26 15:25:26 -0400

    clean up lattice graph and make it a bit more consistant

--- Lattice_Graph.cs
+++ Lattice_Graph.cs
@@ -6,100 +6,84 @@ namespace IsingModel
 {
     class Lattice_Graph
     {
-        private List<List<int>> lattice;           //each element contains a list of neighboring element indices
-        private List<int> ring_population;         //populaiton of each successive ring or corona around the central node
-        private List<int> ring_map;                //map that takes in a site index and returns its ring number
+        private List<List<int>> Lattice;           //each element contains a list of neighboring element indices
+        private List<int> RingPopulation;         //population of each successive ring or corona around the central node
+        private List<int> RingMap;                //map that takes in a site index and returns its ring number
 
-        //private List<Distance_Handler> handler_list;   //used in the Ising model to determine distance
-
-        public int P, Q, R;                                 //parameters of lattice, P is the number of sides to a polygon, Q is the number of polygons
+        public int P, Q, R;                                 //parameters of Lattice, P is the number of sides to a polygon, Q is the number of polygons
                                                             //that meet at a point, R is the number of rings
-        public int Size { get; }                            //total number of lattice sites
+        public int Size { get; }                            //total number of Lattice sites
 
         public Lattice_Graph(int p, int q, int r_max, bool is_periodic = false)
         {
-            lattice = new List<List<int>>(4000);
-            ring_population = new List<int>();
-            ring_map = new List<int>();
+            Lattice = new List<List<int>>(4000);
+            RingPopulation = new List<int>();
+            RingMap = new List<int>();
 
             P = p;
             Q = q;
             R = r_max;
 
-            populate_lattice();
+            PopulateLattice();
 
-            //periodic boundary condition can only apply to Euclidean lattices for now
+            //periodic boundary condition can only apply to Euclidean Lattices for now
             if (is_periodic && (P - 2) * (Q - 2) == 4)
             {
-                apply_periodic_boundary();
+                ApplyPeriodicBoundary();
             }
 
             Size = 0;
-            for (int i = 0; i < lattice.Count; i++)
-                if (lattice[i].Count > 0)
+            for (int i = 0; i < Lattice.Count; i++)
+                if (Lattice[i].Count > 0)
                     Size++;
         }
 
-        private List<int> reverse(List<int> list)
+        //Add neighbor to Lattice site but repeated pairs aren't allowed
+        private void AddNeighborUnique(int site, int neighbor)
         {
-            List<int> replace = new List<int>();
-            for (int i = list.Count - 1; i >= 0; i--)
-                replace.Add(list[i]);
-
-            return replace;
-        }
-
-        //Add neighbor to lattice site but repeated pairs aren't allowed
-        private void add_neighbor_with_enforced_uniqueness(int site, int neighbor)
-        {
-            if (!has_neighbor(site, neighbor))
+            if (!HasNeighbor(site, neighbor))
             {
-                lattice[site].Add(neighbor);
+                Lattice[site].Add(neighbor);
                 return;
             }
         }
 
-
-        private void remove_neighbor(int site, int neighbor)
+        private void RemoveNeighbor(int site, int neighbor)
         {
-            for (int i = 0; i < lattice[site].Count; i++)
+            for (int i = 0; i < Lattice[site].Count; i++)
             {
-                if (lattice[site][i] == neighbor)
+                if (Lattice[site][i] == neighbor)
                 {
-                    lattice[site].RemoveAt(i);
+                    Lattice[site].RemoveAt(i);
                     return;
                 }
             }
         }
 
         //Remove all neighbors of a site
-        private void isolate(int site)
+        private void Isolate(int site)
         {
-            for (int i = 0; i < lattice[site].Count; i++)
+            for (int i = 0; i < Lattice[site].Count; i++)
             {
-                remove_neighbor(lattice[site][i], site);
+                RemoveNeighbor(Lattice[site][i], site);
             }
 
-            lattice[site].Clear();
+            Lattice[site].Clear();
         }
 
-        //Return true if lattice element at site contains neighbor
-        private bool has_neighbor(int site, int neighbor)
+        //Return true if Lattice element at site contains neighbor
+        private bool HasNeighbor(int site, int neighbor)
         {
-            for (int i = 0; i < lattice[site].Count; i++)
-                if (lattice[site][i] == neighbor)
-                    return true;
-
-            return false;
+            return Lattice[site].Contains(neighbor);
         }
 
         //Take two sides of a polygon and add the neighbors that they share to each other
-        private void connect(List<int> sideA, List<int> sideB)
+        private void Connect(List<int> sideA, List<int> sideB)
         {
             if (sideA.Count != sideB.Count)
                 return;
 
-            sideA = reverse(sideA);
+            sideA.Reverse();
 
             int side_length = sideA.Count;
 
@@ -108,40 +92,40 @@ namespace IsingModel
                 int a = sideA[i];
                 int b = sideB[i];
 
-                List<int> neighbor_a = lattice[a];
-                List<int> neighbor_b = lattice[b];
+                List<int> neighbor_a = Lattice[a];
+                List<int> neighbor_b = Lattice[b];
 
                 for (int j = 0; j < neighbor_b.Count; j++)
                 {
-                    add_neighbor_with_enforced_uniqueness(a, neighbor_b[j]);
-                    add_neighbor_with_enforced_uniqueness(neighbor_b[j], a);
+                    AddNeighborUnique(a, neighbor_b[j]);
+                    AddNeighborUnique(neighbor_b[j], a);
                 }
 
                 for (int j = 0; j < neighbor_a.Count; j++)
                 {
-                    add_neighbor_with_enforced_uniqueness(b, neighbor_a[j]);
-                    add_neighbor_with_enforced_uniqueness(neighbor_a[j], b);
+                    AddNeighborUnique(b, neighbor_a[j]);
+                    AddNeighborUnique(neighbor_a[j], b);
                 }
             }
 
-            sideA = reverse(sideA);
+            sideA.Reverse();
         }
 
-        //In a completely generated lattice each site must have Q neighbors 
-        public bool is_complete()
+        //In a completely generated Lattice each site must have Q neighbors 
+        public bool IsComplete()
         {
-            for (int i = 0; i < lattice.Count; i++)
+            for (int i = 0; i < Lattice.Count; i++)
             {
-                if (lattice[i].Count != Q && lattice[i].Count != 0)
+                if (Lattice[i].Count != Q && Lattice[i].Count != 0)
                     return false;
             }
 
             return true;
         }
 
-        private void apply_periodic_boundary()
+        private void ApplyPeriodicBoundary()
         {
-            //periodic boundary conditions aren't ready for hyperbolic lattices
+            //periodic boundary conditions aren't ready for hyperbolic Lattices
             if ((Q - 2) * (P - 2) != 4)
                 return;
 
@@ -152,7 +136,7 @@ namespace IsingModel
             else
                 S = 6;
 
-            int N = ring_population[ring_population.Count - 1];
+            int N = RingPopulation[RingPopulation.Count - 1];
 
             int L = (N / S) + 1;
 
@@ -160,7 +144,7 @@ namespace IsingModel
 
             List<int> perim = new List<int>();
 
-            for (int i = lattice.Count - 1; i >= (lattice.Count - N); i--)
+            for (int i = Lattice.Count - 1; i >= (Lattice.Count - N); i--)
                 perim.Add(i);
 
             for (int i = 0; i < N; i += (L - 1))
@@ -173,184 +157,147 @@ namespace IsingModel
             }
 
             for (int i = 0; i < S; i++)
-                connect(sides[i], sides[(i + S / 2) % S]);
+                Connect(sides[i], sides[(i + S / 2) % S]);
 
-            for (int i = 0; (i < perim.Count) && !is_complete(); i++)
-                isolate(perim[i]);
+            for (int i = 0; (i < perim.Count) && !IsComplete(); i++)
+                Isolate(perim[i]);
         }
 
-        //public List<Double> get_distance_list(int index) { return handler_list[index].get_distances(); }
+        public List<List<int>> GetVertices() { return Lattice; }
+        public List<int> GetRingPopulation() { return RingPopulation; }
+        public List<int> GetRingMap() { return RingMap; }
 
-        /*public void read_in_distance_handlers(String s) throws FileNotFoundException
+        //Generate the Lattice
+        private void PopulateLattice()
         {
-            handler_list = new List<Distance_Handler>(lattice.Count);
-           
-        Scanner in = new Scanner(new File(s));
-        
-        int dist_size, list_size, site;
-        double dist;
+            PopulateZerothRing();
 
-        int total = in.nextInt();
-        
-        for(int i = 0; i<lattice.Count; i++)
-        {
-            dist_size = in.nextInt();
-        Distance_Handler handle = new Distance_Handler();
-
-            for(int k = 0; k<dist_size; k++)
-            {
-                dist = in.nextInt();
-        list_size = in.nextInt();
-
-        List<int> sites = new List<int>(list_size);
-
-                for(int j = 0; j<list_size; j++)
-                    site = in.nextInt();
-
-        handle.Add(dist, sites);
-            }
-
-    handler_list.Add(handle);
-        }
-    }*/
- 
-    
-        public List<List<int>> get_vertices() { return lattice; }
-        public List<int> get_ring_population() { return ring_population; }
-        public List<int> get_ring_map() { return ring_map; }
-
-        //Generate the lattice
-        private void populate_lattice()
-        {
-            populate_zeroth_ring();
-
-            ring_population.Add(1);
+            RingPopulation.Add(1);
 
             if (R == 0)
             {
                 return;
             }
 
-            populate_first_ring();
+            PopulateFirstRing();
 
             int current_ring_start = 1;
             int current_ring_end;
 
-            //Go ring by ring and generate new lattice sites, connecting the next to the current and current to the previous
+            //Go ring by ring and generate new Lattice sites, connecting the next to the current and current to the previous
             for (int r = 1; r < R; r++)
             {
-                current_ring_end = lattice.Count;
+                current_ring_end = Lattice.Count;
 
                 //Go through each site in the current ring and generate a polygon splitting off from it,
                 //these new sites will become the next ring
                 for (int site = current_ring_start; site < current_ring_end; site++)
                 {
-                    extend_corner(lattice[site][0], site, lattice[site][1]);
+                    ExtendCorner(Lattice[site][0], site, Lattice[site][1]);
                 }
 
-                ring_population.Add(current_ring_end - current_ring_start);
+                RingPopulation.Add(current_ring_end - current_ring_start);
 
                 current_ring_start = current_ring_end;
             }
 
-            ring_population.Add(lattice.Count - current_ring_start);
+            RingPopulation.Add(Lattice.Count - current_ring_start);
 
             int sum_so_far = 1;
 
             //Going around the ring creates an error when it gets back to the original site of the ring
-            //this fixes that
-            for (int i = 1; i < ring_population.Count; i++)
+            for (int i = 1; i < RingPopulation.Count; i++)
             {
-                correct_joint(sum_so_far, sum_so_far + ring_population[i] - 1);
-                sum_so_far += ring_population[i];
+                CorrectJoint(sum_so_far, sum_so_far + RingPopulation[i] - 1);
+                sum_so_far += RingPopulation[i];
             }
 
             //Fill in the ring map
-            for (int i = 0; i < ring_population.Count; i++)
+            for (int i = 0; i < RingPopulation.Count; i++)
             {
-                for (int j = 0; j < ring_population[i]; j++)
+                for (int j = 0; j < RingPopulation[i]; j++)
                 {
-                    ring_map.Add(i);
+                    RingMap.Add(i);
                 }
             }
         }
 
         //In a few known places the neighbor lists are in the incorrect order, which creates problems
         //when it comes to rendering them graphically
-        private void correct_joint(int start, int end)
+        private void CorrectJoint(int start, int end)
         {
             int index_a = 0, index_b = 0;
             int temp = 0;
 
-            for (int i = 0; i < lattice[start].Count; i++)
+            for (int i = 0; i < Lattice[start].Count; i++)
             {
-                if (lattice[start][i] == (start + 1))
+                if (Lattice[start][i] == (start + 1))
                     index_a = i;
-                if (lattice[start][i] == end)
+                if (Lattice[start][i] == end)
                     index_b = i;
             }
 
-            temp = lattice[start][index_a];
-            lattice[start][index_a] = lattice[start][index_b];
-            lattice[start][index_b] = temp;
+            temp = Lattice[start][index_a];
+            Lattice[start][index_a] = Lattice[start][index_b];
+            Lattice[start][index_b] = temp;
 
-            for (int i = 0; i < lattice[end].Count; i++)
+            for (int i = 0; i < Lattice[end].Count; i++)
             {
-                if (lattice[end][i] == (end - 1))
+                if (Lattice[end][i] == (end - 1))
                     index_a = i;
-                if (lattice[end][i] == start)
+                if (Lattice[end][i] == start)
                     index_b = i;
             }
 
-            temp = lattice[end][index_a];
-            lattice[end][index_a] = lattice[end][index_b];
-            lattice[end][index_b] = temp;
+            temp = Lattice[end][index_a];
+            Lattice[end][index_a] = Lattice[end][index_b];
+            Lattice[end][index_b] = temp;
 
-            for (int i = 0; i < lattice[start - 1].Count; i++)
+            for (int i = 0; i < Lattice[start - 1].Count; i++)
             {
-                if (lattice[start - 1][i] == start)
+                if (Lattice[start - 1][i] == start)
                 {
                     index_a = i;
                     break;
                 }
             }
 
-            temp = lattice[start - 1][index_a];
+            temp = Lattice[start - 1][index_a];
 
             for (int i = index_a; i > 0; i--)
-                lattice[start - 1][i] = lattice[start - 1][i - 1];
+                Lattice[start - 1][i] = Lattice[start - 1][i - 1];
 
-            lattice[start - 1][0] = temp;
+            Lattice[start - 1][0] = temp;
         }
 
-        private void populate_zeroth_ring()
+        private void PopulateZerothRing()
         {
-            lattice.Add(new List<int>());
+            Lattice.Add(new List<int>());
         }
 
-        private void populate_first_ring()
+        private void PopulateFirstRing()
         {
             int num_new_vertices = Q * (P - 2); // Number of vertices to be added
 
             // Create enough new vertices for the first ring
             for (int i = 0; i < num_new_vertices; i++)
             {
-                lattice.Add(new List<int>());
+                Lattice.Add(new List<int>());
             }
 
             // Connect each new site to the one in front of it, looping around back to itself
             for (int i = 0; i < num_new_vertices; i++)
-                connect(i + 1, (i + 1) % num_new_vertices + 1);
+                Connect(i + 1, (i + 1) % num_new_vertices + 1);
 
             // Connect all the branch vertices to the origin
             for (int i = 1; i <= num_new_vertices; i += (P - 2))
-                connect(0, i);
+                Connect(0, i);
         }
 
-        private void extend_corner(int left, int current, int right)
+        private void ExtendCorner(int left, int current, int right)
         {
             int spanning = P - 3;                        // How many vertices lie between branches
-            int branches = Q - lattice[current].Count;   // How many branch vertices there are (how many more neighbors needed)
+            int branches = Q - Lattice[current].Count;   // How many branch vertices there are (how many more neighbors needed)
             int num_new_vertices = branches + (branches + 1) * spanning; // Number of new vertices
 
             // If current's highest neighbors are on the same ring as current,
@@ -364,35 +311,35 @@ namespace IsingModel
             // until a receptive site is found
             if (branches == 0)
             {
-                while (lattice[right].Count == Q)
+                while (Lattice[right].Count == Q)
                 {
                     num_new_vertices--;
-                    right = lattice[right][0];
+                    right = Lattice[right][0];
                 }
             }
 
-            // Indices describing where the next ring portion goes in the lattice list
-            int next_ring_start = lattice.Count;
-            int next_ring_end = lattice.Count + num_new_vertices - 1;
+            // Indices describing where the next ring portion goes in the Lattice list
+            int next_ring_start = Lattice.Count;
+            int next_ring_end = Lattice.Count + num_new_vertices - 1;
 
             // Create enough vertices to contain the next ring portion
             for (int i = 0; i < num_new_vertices; i++)
-                lattice.Add(new List<int>());
+                Lattice.Add(new List<int>());
 
             // Connect each new site the one in front of it
             for (int i = 0; i < (num_new_vertices - 1); i++)
-                connect(next_ring_start + i, next_ring_start + i + 1);
+                Connect(next_ring_start + i, next_ring_start + i + 1);
 
             // Connect each branch site to the current site
             for (int i = spanning; i < num_new_vertices; i += (spanning + 1))
-                connect(current, next_ring_start + i);
+                Connect(current, next_ring_start + i);
 
             // Connect the dangling ends of the ring portion to the current site's bounding neighbors
-            connect(left, next_ring_start);
-            connect(right, next_ring_end);
+            Connect(left, next_ring_start);
+            Connect(right, next_ring_end);
         }
 
-        private void add_neighbor(List<int> vertex, int neighbor)
+        private void AddNeighbor(List<int> vertex, int neighbor)
         {
             int index;
 
@@ -403,10 +350,10 @@ namespace IsingModel
             vertex.Insert(index, neighbor);
         }
 
-        private void connect(int i, int j)
+        private void Connect(int i, int j)
         {
-            add_neighbor(lattice[i], j);
-            add_neighbor(lattice[j], i);
+            AddNeighbor(Lattice[i], j);
+            AddNeighbor(Lattice[j], i);
         }
     }
 }
